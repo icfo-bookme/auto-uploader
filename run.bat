14cf498a0d205947271a8f8239828c257ea439d7
icfo-bookem <nationalambulancebd@gmail.com>
2025-12-11 16:32:34 +0600

    added

--- run.bat
+++ run.bat
@@ -3,7 +3,8 @@ REM Navigate to the folder where your script is
 cd /d "D:\your-folder-with-script"
 
 REM Run the Node.js script
-node upload-watcher.js
+node D:\auto-upload\upload-watcher.js
 
 REM Keep the window open
 pause
+ 
